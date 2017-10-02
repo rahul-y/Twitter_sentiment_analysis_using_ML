f5d85f92bcbe08af4fac75c0c23e16f3059d229c
Rahul <ry9910725@gmail.com>
2017-10-02 07:57:22 +0530

    update fullcode.py

--- FullCode.py
+++ FullCode.py
@@ -12,7 +12,7 @@ auth.set_access_token(access_token,access_token_secret)
 
 api=tweepy.API(auth)
 
-public_tweets=api.search('cricket')
+public_tweets=api.search('cricket')   #search for word in the tweets which you want
 for tweet in public_tweets:
     print(tweet.text)
     analysis=TextBlob(tweet.text)
